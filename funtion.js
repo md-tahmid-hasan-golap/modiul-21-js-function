be78c2bf9bb2c872f54ab3bcd45f2769e09a6291
golap-raj <golapraj47@gmail.com>
2025-02-10 05:24:59 -0600

    add function

---
+++ funtion.js
@@ -0,0 +1,26 @@
+// function diclaration
+
+function persone (){
+    console.log('my nam is a goalp');
+    console.log('my father name is a abdullah');
+    console.log('my mother name is a chemely')
+}
+// persone();
+// const golap = persone();
+// console.log(golap)
+
+console.log(2*5);
+console.log(10 - 2);
+persone()
+
+
+// for example function diclaration
+
+function sum (x, y){
+    const  result = x + y;
+    return result;
+
+}
+
+const total = sum(5, 5);
+console.log(total)
