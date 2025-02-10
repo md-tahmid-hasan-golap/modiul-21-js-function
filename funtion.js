37ee4d94d8d5c74a86708259426ae020a5ea8f2d
golap-raj <golapraj47@gmail.com>
2025-02-10 13:08:44 -0600

    funxctionm

--- funtion.js
+++ funtion.js
@@ -1,26 +1,56 @@
-// function diclaration
+// // function diclaration
 
-function persone (){
-    console.log('my nam is a goalp');
-    console.log('my father name is a abdullah');
-    console.log('my mother name is a chemely')
+// function persone (){
+//     console.log('my nam is a goalp');
+//     console.log('my father name is a abdullah');
+//     console.log('my mother name is a chemely')
+// }
+// // persone();
+// // const golap = persone();
+// // console.log(golap)
+
+// console.log(2*5);
+// console.log(10 - 2);
+// persone()
+
+
+// // for example function diclaration
+
+// function sum (x, y){
+//     const  result = x + y;
+//     return result;
+
+// }
+
+// const total = sum(5, 5);
+// console.log(total)
+
+
+// function square (a){
+//     const result = a * a;
+//     console.log(result)
+
+// }
+// square(104);
+
+// function numbers (number){
+//     const borgo = number * number;
+//     console.log(borgo);
+
+// }
+// numbers(100);
+// numbers(20);
+// numbers(49);
+
+function add (a, b) {
+    const sum = a + b;
+    console.log(sum)
 }
-// persone();
-// const golap = persone();
-// console.log(golap)
+add(100, 20)
 
-console.log(2*5);
-console.log(10 - 2);
-persone()
-
-
-// for example function diclaration
-
-function sum (x, y){
-    const  result = x + y;
-    return result;
-
+function addAll (a, b, c, d, e){
+    const total = a * b + c * d +e;
+    console.log(total);
 }
-
-const total = sum(5, 5);
-console.log(total)
+addAll(12, 22, 55, 75, 43);
+addAll(1, 3, 5, 7, 2)
---
+++ paramitar.js
@@ -0,0 +1,31 @@
+// function square (number){
+//         console.log(number)
+//         const borgo = number * number;
+//         console.log(borgo)
+// }
+// square(5);
+// square(10);
+// square(15);
+// square(20)
+
+// function addNumber (a, b, c, d, e){
+//     const sum = a + b + c + d + e;
+//     console.log(a, b, c, d, e)
+//     console.log(sum);
+
+// }
+
+// addNumber(10, 3, 4, 22, 66);
+
+function doubleds (number){
+    const doubled = number * number;
+    console.log(number, doubleds);
+}
+doubleds(20);
+doubleds(40)
+
+for(let i = 1; i <=50; i++){
+    if(i % 3 === 0){
+        console.log(i)
+    }
+}
