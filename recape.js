c1d58a2162e1ce53213676394d0cb6cd7947a685
golap-raj <golapraj47@gmail.com>
2025-02-11 11:52:26 -0600

    ddd

---
+++ recape.js
@@ -0,0 +1,26 @@
+// duble koro 
+
+function double (i){
+    const result = i * 2;
+    console.log(i, result)
+
+}
+double(66)
+console.log('-----------------')
+double(44);
+console.log('---------------')
+double(22)
+
+const money = 100;
+double(money)
+
+function difirence (num1, num2){
+    const diff = num1 - num2;
+    console.log(diff)
+
+}
+
+const fatherAge = 50;
+const myage = 20;
+difirence(100, 30)
+difirence(fatherAge, myage)
---
+++ return.js
@@ -0,0 +1,16 @@
+// return in a java scripts
+
+function tentimes (number){
+    const result = number * 10;
+    return result
+}
+const output = tentimes(10);
+console.log(output)
+
+function cuthulf(numbers){
+    const half = numbers / 2;
+    return half
+
+}
+const result = cuthulf(100) 
+console.log(result)
