999def50bfa0fb2316720cc8516dbff407561a14
golap-raj <golapraj47@gmail.com>
2025-02-11 10:44:24 -0600

    function add

---
+++ functions.js
@@ -0,0 +1,17 @@
+// function diclaration
+function square (a, b){
+    // console.log("My name is a square")
+    const total = a + b;
+    console.log(total)
+}
+square(11, 66);
+square(83883, 373829292)
+
+
+function biyog (x, y){
+    const total = x - y;
+    console.log(total)
+
+}
+biyog(12334, 6555)
+biyog(50, 40)
